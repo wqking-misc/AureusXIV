4cf4dc822413453a4b37daa4e1301eaf1802fd9c
wqking <wqking@outlook.com>
2021-01-25 21:04:32 +0800

    Revert "Merge pull request #34 from wqking-misc/upstream-testnet"
    This reverts commit 1e516bf788d3b66c0481655a105b38f6130d4af5, reversing
    changes made to db8a50415170ddedb82bb0cce3ceb301f61bf26d.

--- src/fundamentalnode.cpp
+++ src/fundamentalnode.cpp
@@ -339,7 +339,7 @@ bool CFundamentalnode::IsInputAssociatedWithPubkey() const
     uint256 hash;
     if(GetTransaction(vin.prevout.hash, txVin, hash, true)) {
         for (CTxOut out : txVin.vout) {
-            if (out.nValue == FN_MAGIC_AMOUNT && out.scriptPubKey == payee) return true;
+            if (out.nValue == 10000 * COIN && out.scriptPubKey == payee) return true;
         }
     }
 
