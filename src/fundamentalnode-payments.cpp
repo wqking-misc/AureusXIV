f3df42ec6acd83010f5600b7e90b1b17e80ba5bd
wqking <wqking@outlook.com>
2021-02-03 20:20:05 +0800

    Fixed a vtx out of range bug

--- src/fundamentalnode-payments.cpp
+++ src/fundamentalnode-payments.cpp
@@ -245,6 +245,11 @@ bool IsBlockPayeeValid(const CBlock& block, int nBlockHeight)
         MasternodePayments = false; //
         if(fDebug) LogPrintf("CheckBlock() : Masternode payment enforcement is off\n");
     }
+    
+    if(block.vtx.size() < 2) {
+        MasternodePayments = false; //
+        if(fDebug) LogPrintf("CheckBlock() : There is no masternode payment\n");
+    }
 
     if(MasternodePayments)
     {
